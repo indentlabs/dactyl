b98243a7456178643faa1b6dc3596b01206ce347
Andrew Brown <drusepth@gmail.com>
2015-07-16 22:34:39 -0500

    tabs to spaces

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,32 +1,32 @@
 class ApiController < ApplicationController
 
-	before_action :set_analysis_string
+    before_action :set_analysis_string
 
-	def set_analysis_string
-		@analysis_string ||= begin
-			fetch_remote_text   if params[:url].present?
-			fetch_file_contents if params[:upload].present?
+    def set_analysis_string
+        @analysis_string ||= begin
+            fetch_remote_text   if params[:url].present?
+            fetch_file_contents if params[:upload].present?
 
-			params[:string] || params[:text]
-		end
-	end
+            params[:string] || params[:text]
+        end
+    end
 
-	def dactyl
-		return unless @analysis_string.present?
+    def dactyl
+        return unless @analysis_string.present?
 
-		d = Dactylogram.new(data: @analysis_string)
-		d.instance_variable_set(:@metrics, params[:metrics].map { |m| "#{m}_metric" }) if params[:metrics].present?
+        d = Dactylogram.new(data: @analysis_string)
+        d.instance_variable_set(:@metrics, params[:metrics].map { |m| "#{m}_metric" }) if params[:metrics].present?
 
-		render :json => d.metric_report
-	end
+        render :json => d.metric_report
+    end
 
-	private
+    private
 
-	def fetch_file_contents file_upload
-		"#todo support file upload"
-	end
+    def fetch_file_contents file_upload
+        "#todo support file upload"
+    end
 
-	def fetch_remote_text url
-		"#todo support remote text fetching"
-	end
+    def fetch_remote_text url
+        "#todo support remote text fetching"
+    end
 end
--- app/models/dactylogram.rb
+++ app/models/dactylogram.rb
@@ -1,115 +1,115 @@
 class Dactylogram < ActiveRecord::Base
-	attr_accessor :data
+    attr_accessor :data
 
-	SYLLABLE_COUNT_OVERRIDES = {
-		'ion' => 2
-	}
+    SYLLABLE_COUNT_OVERRIDES = {
+        'ion' => 2
+    }
 
-	def metric_report
-		{
-			original_string: data,
-			metrics: calculate_metrics
-		}
-	end
+    def metric_report
+        {
+            original_string: data,
+            metrics: calculate_metrics
+        }
+    end
 
-	def all_metrics
-		self.class.instance_methods.select { |m| metric? m }
-	end
+    def all_metrics
+        self.class.instance_methods.select { |m| metric? m }
+    end
 
-	def average_sentence_length_metric
-		data.length.to_f / sentences.length
-	end
+    def average_sentence_length_metric
+        data.length.to_f / sentences.length
+    end
 
-	def data_length_metric
-		data.length
-	end
+    def data_length_metric
+        data.length
+    end
 
-	def sentences_metric
-		sentences.length
-	end
+    def sentences_metric
+        sentences.length
+    end
 
-	def spaces_after_sentence_metric
-		spaces_per_sentence = sentences.map { |sentence|
-			sentence.length - sentence.lstrip.length
-		}.sum.to_f / (sentences.length - 1)
-	end
+    def spaces_after_sentence_metric
+        spaces_per_sentence = sentences.map { |sentence|
+            sentence.length - sentence.lstrip.length
+        }.sum.to_f / (sentences.length - 1)
+    end
 
-	def syllable_count_metric
-		word_syllables.sum
-	end
+    def syllable_count_metric
+        word_syllables.sum
+    end
 
-	def syllables_per_sentence_metric
-		total_syllables = 0
-		sentences.each do |sentence|
-			total_syllables += sentence.split(' ').map(&method(:syllables_in)).sum
-		end
+    def syllables_per_sentence_metric
+        total_syllables = 0
+        sentences.each do |sentence|
+            total_syllables += sentence.split(' ').map(&method(:syllables_in)).sum
+        end
 
-		total_syllables.to_f / sentences.length
-	end
+        total_syllables.to_f / sentences.length
+    end
 
-	def unique_words_percentage_metric
-		unique_words.length.to_f / words.length
-	end
+    def unique_words_percentage_metric
+        unique_words.length.to_f / words.length
+    end
 
-	def whitespace_percentage_metric
-		occurrences(of: ["\s", "\r", "\n"], within: data.chars).to_f / data.length
-	end
+    def whitespace_percentage_metric
+        occurrences(of: ["\s", "\r", "\n"], within: data.chars).to_f / data.length
+    end
 
-	def word_count_metric
-		words.length
-	end
+    def word_count_metric
+        words.length
+    end
 
-	def words_per_sentence_metric
-		words.length.to_f / sentences.length
-	end
+    def words_per_sentence_metric
+        words.length.to_f / sentences.length
+    end
 
-	# ... more stuffs ... #
+    # ... more stuffs ... #
 
-	private
+    private
 
-	# Given a method name (symbol), return whether it should be ran as a metric
-	def metric? method_name
-		method_name.to_s.end_with? '_metric'
-	end
+    # Given a method name (symbol), return whether it should be ran as a metric
+    def metric? method_name
+        method_name.to_s.end_with? '_metric'
+    end
 
-	def calculate_metrics
-		results = {}
-		(@metrics || all_metrics).map { |metric|
-			results[metric.to_s.chomp '_metric'] = send(metric)
-		}
-		results
-	end
+    def calculate_metrics
+        results = {}
+        (@metrics || all_metrics).map { |metric|
+            results[metric.to_s.chomp '_metric'] = send(metric)
+        }
+        results
+    end
 
-	def sentences
-		data.split(/[!\?\.]/)
-	end
+    def sentences
+        data.split(/[!\?\.]/)
+    end
 
-	def words
-		data.gsub(/[^\s\w]/, '').split(' ')
-	end
+    def words
+        data.gsub(/[^\s\w]/, '').split(' ')
+    end
 
-	def unique_words
-		words.map(&:downcase).uniq
-	end
+    def unique_words
+        words.map(&:downcase).uniq
+    end
 
-	def word_syllables
-		words.map(&method(:syllables_in))
-	end
+    def word_syllables
+        words.map(&method(:syllables_in))
+    end
 
-	def syllables_in word
-		word.downcase.gsub!(/[^a-z]/, '')
+    def syllables_in word
+        word.downcase.gsub!(/[^a-z]/, '')
 
-		return 1 if word.length <= 3
-		return SYLLABLE_COUNT_OVERRIDES[word] if SYLLABLE_COUNT_OVERRIDES.key? word
+        return 1 if word.length <= 3
+        return SYLLABLE_COUNT_OVERRIDES[word] if SYLLABLE_COUNT_OVERRIDES.key? word
 
-		word.sub(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '').sub!(/^y/, '')
-		word.scan(/[aeiouy]{1,2}/).size
-	end
+        word.sub(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '').sub!(/^y/, '')
+        word.scan(/[aeiouy]{1,2}/).size
+    end
 
-	def occurrences of: needles, within: haystack
-		of = [of] unless of.is_a? Array
+    def occurrences of: needles, within: haystack
+        of = [of] unless of.is_a? Array
 
-		within.flatten.select { |hay| of.include? hay }.length
-	end
+        within.flatten.select { |hay| of.include? hay }.length
+    end
 
 end
