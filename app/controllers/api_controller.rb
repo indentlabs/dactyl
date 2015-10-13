32a53bea546e2bbda3541164a5aae3d683fe9fec
Andrew Brown <drusepth@gmail.com>
2015-10-12 19:18:09 -0500

    Closes #2

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,7 @@
 class ApiController < ApplicationController
 
     def dactyl
-        return unless @analysis_string.present?
+        @analysis_string ||= ""
 
         d = Dactylogram.new(data: @analysis_string)
         d.instance_variable_set(:@metrics, params[:metrics].map { |m| "#{m}_metric" }) if params[:metrics].present?
