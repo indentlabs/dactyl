f8f16a0fa2156abd65d6ac9e1e1907ab9c729411
Andrew Brown <drusepth@gmail.com>
2015-12-03 19:36:53 -0600

    metric model

---
+++ db/migrate/20151204013558_create_metrics.rb
@@ -0,0 +1,10 @@
+class CreateMetrics < ActiveRecord::Migration
+  def change
+    create_table :metrics do |t|
+      t.string :name
+      t.string :description
+
+      t.timestamps null: false
+    end
+  end
+end
