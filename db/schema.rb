9822566b1873a50405cbe7fad7ec3cb1be48517c
Andrew Brown <andrew@gomoment.com>
2016-05-24 16:23:29 -0500

    Add oauth columns to users for omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524211848) do
+ActiveRecord::Schema.define(version: 20160524212233) do
 
   create_table "corpus", force: :cascade do |t|
     t.string   "text"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 20160524211848) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
