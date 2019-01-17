7ef589520ed12b9e95a8d254588d7dffd43ff810
Andrew Brown <drusepth@gmail.com>
2019-01-17 16:03:08 -0600

    add user fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_17_220150) do
+ActiveRecord::Schema.define(version: 2019_01_17_220241) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
